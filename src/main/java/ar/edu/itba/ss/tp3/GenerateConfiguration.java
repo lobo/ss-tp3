9b66bd0c37f33cfa5a65bf63bef0cd5b8f60a45c
Daniel Lobo <dlobo@itba.edu.ar>
2018-04-01 12:25:24 -0300

    clean files

--- src/main/java/ar/edu/itba/ss/tp3/GenerateConfiguration.java
+++ src/main/java/ar/edu/itba/ss/tp3/GenerateConfiguration.java
@@ -27,37 +27,7 @@ public final class GenerateConfiguration {
 	private Double deltat = 0.0;
 	private String inputfile = "";
 	private String outputfile = "";
-	
-	/*
-	{
-	"n"			: "10",
-	"events"		: "100",
-	"tmax"		: "60.0",
-	"l"			: "10.0",
-	"r"			: "0.005",
-	"speed"		: "0.1",
-	"mass"		: "0.0001",
-	"rbig"		: "0.05",
-	"massbig"	: "0.1",
-	"xbig"		: "5.0",
-	"ybig"		: "5.0",
-	"inputfile"	: "input1.data",
-	"outputfile"	: "output1.data",
-	"deltat"		: "0.05"
-	
-	final int n = 10000;			// Cantidad de partículas
-	final long e = 100;				// Cantidad máxima de eventos
-	final double tmax = 60.0;		// Tiempo máximo de simulación
-	final double l = 10.0;			// Dimensión del espacio
-	final double r = 0.005;			// Radio de las partículas
-	final double rbig = 0.05;		// Radio distinguido
-	final double speed = 0.1;		// Rapidez máxima
-	final double mass = 0.0001;		// Masa de las partículas (en Kg.)
-	final double massbig = 0.1;		// Masa distinguida (en Kg.)
-	//final double Δt = 0.1;		// Intervalo de animación
-	}
-	 */
-	
+		
 	public Integer getN() {
 		return n;
 	}
--- src/main/java/ar/edu/itba/ss/tp3/SimulateConfiguration.java
+++ src/main/java/ar/edu/itba/ss/tp3/SimulateConfiguration.java
@@ -20,31 +20,6 @@ public final class SimulateConfiguration {
 	private Double deltat = 0.0;
 	private String inputfile = "";
 	private String outputfile = "";
-	
-	/*
-{
-	"events"		: "100",
-	"tmax"		: "60.0",
-	"l"			: "10.0",
-	"inputfile"	: "input1.data",
-	"outputfile"	: "output1.data",
-	"deltat"		: "0.05"
-}
-	
-	final int n = 10000;			// Cantidad de partículas
-	final long e = 100;				// Cantidad máxima de eventos
-	final double tmax = 60.0;		// Tiempo máximo de simulación
-	final double l = 10.0;			// Dimensión del espacio
-	final double r = 0.005;			// Radio de las partículas
-	final double rbig = 0.05;		// Radio distinguido
-	final double speed = 0.1;		// Rapidez máxima
-	final double mass = 0.0001;		// Masa de las partículas (en Kg.)
-	final double massbig = 0.1;		// Masa distinguida (en Kg.)
-	//final double Δt = 0.1;		// Intervalo de animación
-	}
-	 */
-	
-	
 		
 	public static String getConfigurationFilename() {
 		return CONFIGURATION_FILENAME;
